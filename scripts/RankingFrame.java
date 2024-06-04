604c8d285df554d7c6b791b704ef78beb2a48435
Vinter-max <sunvint2828@gmail.com>
2024-06-04 08:16:23 -0400

    Update RankingFrame.java

--- scripts/RankingFrame.java
+++ scripts/RankingFrame.java
@@ -6,28 +6,28 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class RankingFrame extends JFrame implements ActionListener {
-	private JSplitPane mainPanel;
+    private JSplitPane mainPanel;
     private JPanel rankPanel, btnPanel;
-	private JLabel title, ranks;
-	private JButton mainMenu;
-	
-	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new RankingFrame();
-			}
-		});
-	}
-	
-	public RankingFrame() {
-		this.setTitle("Rankings");
+    private JLabel title, ranks;
+    private JButton mainMenu;
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new RankingFrame();
+            }
+        });
+    }
+
+    public RankingFrame() {
+        this.setTitle("Rankings");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //Maximize the window
+
+        // Maximize the window
         this.setExtendedState(MAXIMIZED_BOTH);
         this.setUndecorated(true);
 
-        //press esc to exit
+        // press esc to exit
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
             @Override
             public boolean dispatchKeyEvent(KeyEvent e) {
@@ -38,53 +38,53 @@ public class RankingFrame extends JFrame implements ActionListener {
             }
         });
 
-        //Set the font to be bigger
-        //Change the margin and style of button
-        //Create method to read file record.txt
-        
-        //Main Panel
+        // Set the font to be bigger
+        // Change the margin and style of button
+        // Create method to read file record.txt
+
+        // Main Panel
         mainPanel = new JSplitPane();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         mainPanel.setDividerLocation(760);
-        mainPanel.setBackground(new Color(255, 240, 255));  // Set background color
+        mainPanel.setBackground(new Color(255, 240, 255)); // Set background color
 
-        //Rank Panel
+        // Rank Panel
         rankPanel = new JPanel();
         rankPanel.setLayout(new BoxLayout(rankPanel, BoxLayout.PAGE_AXIS));
         rankPanel.setPreferredSize(new Dimension(1500, 750));
-        rankPanel.setBackground(new Color(255, 240, 255));  // Set background color
+        rankPanel.setBackground(new Color(255, 240, 255)); // Set background color
 
         title = new JLabel("Rankings");
         rankPanel.add(title);
-        
-        ranks = new JLabel("tbd..."); //tbd...
+
+        ranks = new JLabel("tbd..."); // tbd...
         rankPanel.add(ranks);
-        
-        //Button Panel for return button
+
+        // Button Panel for return button
         btnPanel = new JPanel();
         btnPanel.setLayout(new BorderLayout(10, 10));
         btnPanel.setPreferredSize(new Dimension(1500, 40));
-        btnPanel.setBackground(new Color(255, 240, 255));  // Set background color
+        btnPanel.setBackground(new Color(255, 240, 255)); // Set background color
 
         mainMenu = new JButton("Return to Main Menu");
         mainMenu.setActionCommand("return");
         mainMenu.addActionListener(this);
         btnPanel.add(mainMenu, BorderLayout.PAGE_END);
-        
+
         mainPanel.setTopComponent(rankPanel);
         mainPanel.setBottomComponent(btnPanel);
         this.add(mainPanel);
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String event = e.getActionCommand();
-		
-		if(event.equals("return")) {
-	new MainFrame();
-		}
-	}
-}
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String event = e.getActionCommand();
+
+        if (event.equals("return")) {
+            this.setVisible(false);
+            new MainFrame();
+        }
+    }
+}
